9877b6a78cadc8f88f65814128b729e1038800c6
BEUMGOO <captain_kbg@naver.com>
2021-02-27 03:03:36 +0900

    Prisma Client

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 #Instaclone Backend
 
+npx prisma migrate dev --preview-feature
+
 todo
 0. xxx
 0. xxx
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server"; 
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
   type Movie {
     id: Int
     title: String
     year: Int
+    genre: String
+    CreatedAt: String
+    UpdatedAt: String
   }
   type Query {
     movies: [Movie]
     movie: Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
+    createMovie(title: String!, year:Int!, genre:String): Movie
     deleteMovie(title: String!): Boolean
   }
 `;
@@ -19,14 +25,15 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    movies: () => [],
+    movies: () => client.movie.findMany(),
     movie: () => ({ "title": "xman", year: 2020 })
   },
   Mutation: {
-    createMovie: (_, {title}) => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_, { title, year, genre }) => client.movie.create({data: {
+      title,
+      year,
+      genre
+    }}),
     deleteMovie: (_, {title}) => {
       console.log(title);
       return true;
