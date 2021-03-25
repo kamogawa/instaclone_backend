d460bb7f3e536f9e0ac1544b56a2148e491ea9b6
BEUMGOO <captain_kbg@naver.com>
2021-03-25 22:48:23 +0900

    login

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,28 +7,52 @@ export default {
       _, 
       { firstName, lastName, username, email, password }
     )　=> {
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email
-            }
-          ]
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email
+              }
+            ]
+          }
+        });
+        if (existingUser) {
+          throw new Error("This username/email is already taken.");
         }
-      });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      return client.user.create({ 
-        data: {
-          username,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword
-        },
-      });
-    }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({ 
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword
+          },
+        });
+      } catch (error) {
+        return error;
+      }
+    },
+    login: async(_, {username, password}) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not fount"
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect Password"
+        };
+      }
+      return "test"
+    },
   }
 }
--- users/users.queries.js
+++ users/users.queries.js
@@ -1,5 +1,12 @@
+import client from "../client";
+
 export default {
   Query: {
-    
+    seeProfile: (_, {username}) => 
+      client.user.findUnique({
+        where: {
+          username,
+        },
+      }),
   }
 }
